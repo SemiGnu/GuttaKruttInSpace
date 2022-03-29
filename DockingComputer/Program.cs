cb2095a50491236fd1743ec346aaeae3cd0fb092
Torstein Winjum <twinjum@hotmail.com>
2022-03-30 01:29:38 +0200

    Remove pid

--- DockingComputer/Program.cs
+++ DockingComputer/Program.cs
@@ -104,50 +104,11 @@ namespace IngameScript
                 CalculateTargetVector();
                 CalculateRotationVector();
                 SetGyros();
-                //ControlThrust();
                 TryDocking();
                 _dockingLcd.WriteText(GetStatus());
             }
         }
-        private void Translate() {
-            if (!_auto || _angle > 0.01) return;
-            if (!_translating)
-            {
-                _translating = true;
-                _startVector = _targetVector / 2;
 
-            }
-        }
-
-        //private void ControlThrust()
-        //{
-        //    if (!_auto || _angle > 0.01) return;
-        //    var xErr = - _targetVector.X / _startVector.X;
-        //    var t = (float)_x.Control(xErr);
-        //    _xx = t;
-        //    Echo($"{xErr}");
-        //    SetThrust(t, "X");
-        //}
-
-        //private void SetThrust(float thrust, string direction)
-        //{
-        //    switch(direction)
-        //    {
-        //        case "X":
-        //            if (thrust > 0)
-        //            {
-        //                _upThrusters.ForEach(t => t.ThrustOverridePercentage = Math.Max(thrust);
-        //            } else
-        //            {
-        //                _downThrusters.ForEach(t => t.ThrustOverridePercentage = -thrust);
-        //            }
-        //            break;
-        //        default:
-        //            break;
-
-        //    }
-            
-        //}
 
         private void HandleAutoTrigger()
         {
@@ -307,74 +268,6 @@ namespace IngameScript
                     return trans + (magnitude < 0 ? "Rgt" : "Lft");
                 default:
                     return trans + (magnitude < 0 ? "Rgt" : "Lft");
-            }
-        }
-
-        public class PID
-        {
-            readonly double _kP = 0;
-            readonly double _kI = 0;
-            readonly double _kD = 0;
-
-            double _timeStep = 0;
-            double _inverseTimeStep = 0;
-            double _errorSum = 0;
-            double _lastError = 0;
-            bool _firstRun = true;
-
-            public double Value { get; private set; }
-
-            public PID(double kP, double kI, double kD, double timeStep)
-            {
-                _kP = kP;
-                _kI = kI;
-                _kD = kD;
-                _timeStep = timeStep;
-                _inverseTimeStep = 1 / _timeStep;
-            }
-
-            protected virtual double GetIntegral(double currentError, double errorSum, double timeStep)
-            {
-                return errorSum + currentError * timeStep;
-            }
-
-            public double Control(double error)
-            {
-                //Compute derivative term
-                var errorDerivative = (error - _lastError) * _inverseTimeStep;
-
-                if (_firstRun)
-                {
-                    errorDerivative = 0;
-                    _firstRun = false;
-                }
-
-                //Get error sum
-                _errorSum = GetIntegral(error, _errorSum, _timeStep);
-
-                //Store this error as last error
-                _lastError = error;
-
-                //Construct output
-                this.Value = _kP * error + _kI * _errorSum + _kD * errorDerivative;
-                return this.Value;
-            }
-
-            public double Control(double error, double timeStep)
-            {
-                if (timeStep != _timeStep)
-                {
-                    _timeStep = timeStep;
-                    _inverseTimeStep = 1 / _timeStep;
-                }
-                return Control(error);
-            }
-
-            public void Reset()
-            {
-                _errorSum = 0;
-                _lastError = 0;
-                _firstRun = true;
             }
         }
     }
